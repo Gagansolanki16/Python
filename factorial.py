39b525cb7eaf013c72e623d237b7f91d1f64b5d2
Gagan Solanki <gagansolanki16053@gmail.com>
2025-02-09 21:22:08 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,18 @@
+
+# a=int(input("Enter a number:"))
+
+# f=a
+
+# for i in range(a,1,-1):
+#     f*=i-1
+
+
+# print("The factorial of",a,"is",f)
+
+import math
+
+a=int(input("Enter a number:"))
+
+b=math.factorial(a)
+
+print(b)
