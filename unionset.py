a26a0d6e78afcf9c7ff1a890e522e80b0cc861e3
Gagan Solanki <gagansolanki16053@gmail.com>
2025-02-12 21:43:58 +0530

    Add files via upload

---
+++ unionset.py
@@ -0,0 +1,10 @@
+set={2,11,55,92,23,12,88,8,11,20}
+
+set1={11,22,1,18,99,61,44,5,74,22}
+
+
+set2=set.union (set1)
+
+set3=set.intersection (set1)
+print("Union of set is: ", set2)
+print("Intersection of set is: ", set3)
